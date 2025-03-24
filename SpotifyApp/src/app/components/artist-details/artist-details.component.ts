f0f4df0d0a43fe63de77dd51d5a76800d2b82057
Jakub-Lukac <jakub.lukac81@gmail.com>
2025-03-24 12:49:37 +0000

    artist details card style adjusted

--- SpotifyApp/src/app/components/artist-details/artist-details.component.ts
+++ SpotifyApp/src/app/components/artist-details/artist-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Artist } from '../../models/artist-response';
 
 @Component({
   selector: 'app-artist-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './artist-details.component.html',
   styleUrl: './artist-details.component.scss'
 })
